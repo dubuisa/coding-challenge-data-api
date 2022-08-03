34f4b5842946ddda94c28d91975a4358f708066a
dubuisa <antoine.dubuis@gmail.com>
2022-08-03 20:43:38 +0200

    reformat script with black

--- app/migrations/versions/9947d2b2e466_init_database.py
+++ app/migrations/versions/9947d2b2e466_init_database.py
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9947d2b2e466'
+revision = "9947d2b2e466"
 down_revision = None
 branch_labels = None
 depends_on = None
--- app/migrations/versions/f9c634db477d_create_dialog_table.py
+++ app/migrations/versions/f9c634db477d_create_dialog_table.py
@@ -10,26 +10,27 @@ import sqlalchemy as sa
 import sqlmodel
 
 # revision identifiers, used by Alembic.
-revision = 'f9c634db477d'
-down_revision = '9947d2b2e466'
+revision = "f9c634db477d"
+down_revision = "9947d2b2e466"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.create_table('dialog',
-    sa.Column('id', sa.Integer(), nullable=True, autoincrement=True, primary_key=True),
-    sa.Column('dialogId', sa.Integer(), nullable=False),
-    sa.Column('customerId', sa.Integer(), nullable=False),
-    sa.Column('text', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
-    sa.Column('language', sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+    op.create_table(
+        "dialog",
+        sa.Column("id", sa.Integer(), autoincrement=True, primary_key=True),
+        sa.Column("dialogId", sa.Integer(), nullable=False),
+        sa.Column("customerId", sa.Integer(), nullable=False),
+        sa.Column("text", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
+        sa.Column("language", sqlmodel.sql.sqltypes.AutoString(), nullable=False),
     )
 
-    op.create_index(op.f('ix_dialog_id'), 'dialog', ['id'], unique=False)
-    op.create_index(op.f('ix_dialog_dialogId'), 'dialog', ['id'], unique=False)
+    op.create_index(op.f("ix_dialog_id"), "dialog", ["id"], unique=False)
+    op.create_index(op.f("ix_dialog_dialogId"), "dialog", ["id"], unique=False)
 
 
 def downgrade():
-    op.drop_index(op.f('ix_dialog_id'), table_name='dialog')
-    op.drop_index(op.f('ix_dialog_dialogId'), table_name='dialog')
-    op.drop_table('dialog')
+    op.drop_index(op.f("ix_dialog_id"), table_name="dialog")
+    op.drop_index(op.f("ix_dialog_dialogId"), table_name="dialog")
+    op.drop_table("dialog")
